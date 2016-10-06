80009c4d7cd8d6223a7f8395b4b4e5bb1b0792be
Charles Lynch <charlesloper2001@gmail.com>
2016-10-06 14:46:31 -0700

    Add files via upload

--- ComputeTax.cpp
+++ ComputeTax.cpp
@@ -1,67 +1,70 @@
 #include <iostream>
-#include <string>
-
+#include<string>
+//#include "Income.cpp"
 
 using namespace std;
 
-class ComputeTax
+
+int hello()
 {
-	int hello()
+	cout << "+--------------------------------------------+" << endl;
+	cout << "|                                            |" << endl;
+	cout << "|     Highline College C++ Assignment 2      |" << endl;
+	cout << "|        Fake income tax calculator          |" << endl;
+	cout << "|     Copyright (C) 2016 Charles Lynch       |" << endl;
+	cout << "|                                            |" << endl;
+	cout << "+--------------------------------------------+" << endl;
+	return 0;
+}
+
+int columns()
+{
+	int columns = 0;
+	string answer = "";
+	cout << "How many columns would you liked to display? ";
+	cin >> columns;
+	if (columns <= 40)
 	{
-		cout << "+--------------------------------------------+" << endl;
-		cout << "|                                            |" << endl;
-		cout << "|     Highline College C++ Assignment 2      |" << endl;
-		cout << "|        Fake income tax calculator          |" << endl;
-		cout << "|     Copyright (C) 2016 Charles Lynch       |" << endl;
-		cout << "|                                            |" << endl;
-		cout << "+--------------------------------------------+" << endl;
+		cout << "You must have 40 columns or more!" << endl;
+		return -1;
 	}
 
-	int columns()
+	else
 	{
-		int columns = 0;
-		string answer = "";
-		cout << "How many columns would you liked to display?";
-		cin >> columns;
-		if (columns <= 40)
-		{
-			cout << "You must have 40 columns or more!";
-		}
+		cout << "Confirm you entered " << columns << " columns (type Yes or No) ? ";
+		cin >> answer;
 
-		else
+		if (answer == "Yes")
 		{
-			cout << "Confirm you entered " << columns << " columns (type Yes or No) ?";
-			cin >> answer;
-
-			if (answer == "Yes")
+			for (int i = 0; i < columns; i++)
 			{
-				for (int i = 0; i < columns; i++)
-				{
-					cout << "-";
-				}
+				cout << "-";
 			}
-
-			else
-			{
-				return -1;
-			}	
 		}
-		return 0;
-	}
 
-	int main()
-	{
-		hello();
-		if (columns() == 0)
+		else if (answer == "No")
 		{
-
+			return -1;
 		}
 
 		else
 		{
-			columns();
+			cout << "You must enter a Yes of No" << endl;
+			return -1;
 		}
-	
-		return 0;
 	}
-};
+	return 0;
+}
+
+
+int main()
+{
+	hello();
+
+	while (columns() != 0)
+	{
+		columns();
+	}
+
+	return 0;
+}
